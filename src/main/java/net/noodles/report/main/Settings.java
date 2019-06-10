5b434b6f8f3ce2c70710d3a1670015f9fb13de58
StephanBH <bghdpayments@gmail.com>
2019-06-10 08:37:28 -0700

    Fixed things

--- src/main/java/net/noodles/report/main/Settings.java
+++ src/main/java/net/noodles/report/main/Settings.java
@@ -4,7 +4,7 @@ public class Settings {
 
     public static String DEVELOPER_NAME = "BGHDDevelopment";
     public static String DEVELOPER_URL = "https://bghddevelopment.com";
-    public static String PLUGIN_URL = "https://spigotmc.org/resources/";
+    public static String PLUGIN_URL = "https://spigotmc.org/resources/68177";
     public static String SUPPORT_DISCORD_URL = "https://bghddevelopment.com/discord";
-    public static String VERSION = "0.1";
+    public static String VERSION = "0.2";
 }
